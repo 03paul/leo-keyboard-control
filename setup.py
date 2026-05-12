c5634a958e5af3fda08b0aea45fe1ba62d5405d5
03paul_ <diasjdiasjd@sada>
2026-05-12 14:41:27 +0200

    asdkasdn

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'keyboard_control'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Paul',
+    maintainer_email='none@example.com',
+    description='Keyboard control for Leo Rover',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'keyboard_node = keyboard_control.keyboard_node:main',
+        ],
+    },
+)
